635defdb114fe1b7c58687ab3871b94606934490
KatlaAchintya <73953374+KatlaAchintya@users.noreply.github.com>
2020-12-09 18:50:27 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,25 @@
+class Bob{
+    constructor(x, y, radius) {
+        var options = {
+            isStatic: false,
+            friction: 0.5,
+            density: 1,
+            restitution: 0.8
+        }
+
+        this.body = Bodies.circle(x, y, 30, options);
+
+        this.x = x;
+        this.y = y;
+
+        World.add(world,this.body);
+    }
+    display(){
+        ellipseMode(RADIUS)
+        push();
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        ellipse(0,0, 30, 30);
+        pop();
+    }
+}
